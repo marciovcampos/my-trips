c2491bab22c6d56227ae50c5f3eb5eacb6271f5b
Márcio Vinícius <marcio.campos@outlook.com>
2021-04-18 14:00:13 -0300

    fix: fix global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
-import LinkWrapper from 'components/LinkWrapper'
 import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+import LinkWrapper from 'components/LinkWrapper'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,43 +5,35 @@ const GlobalStyles = createGlobalStyle`
     --highlight: #e20e8d;
     --background: #030518;
     --white: #eeeeee;
-
     --container: 100rem;
-
     --small: 1.5rem;
-    --medim: 3rem;
+    --medium: 3rem;
     --large: 5rem;
   }
-
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-
   html {
     font-size: 62.5%;
   }
-
   html, body, #__next {
     height: 100%;
     background: var(--background);
     color: var(--white);
   }
-
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
-
-  p, a {
+  p,
+  a {
     font-size: 2rem;
     line-height: var(--medium);
   }
-
-  a{
+  a {
     color: var(--highlight);
   }
-
 `
 
 export default GlobalStyles
