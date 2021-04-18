c7ad1685dc18e2d8cb79764a4a3bdd9a3ac55dba
Márcio Vinícius <marcio.campos@outlook.com>
2021-04-18 17:48:28 -0300

    create generic PageTemplate to N Pages

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -2,24 +2,20 @@ import LinkWrapper from 'components/LinkWrapper'
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 import * as S from './styles'
 
-const PageTemplate = () => {
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => {
   return (
     <S.Content>
       <LinkWrapper href="/">
         <CloseOutline />
       </LinkWrapper>
-      <S.Heading>My Trips</S.Heading>
-
+      <S.Heading>{heading}</S.Heading>
       <S.Body>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed
-          rutrum massa, ut gravida tortor. Vestibulum et velit eget purus
-          bibendum dapibus id pellentesque orci. Sed vehicula, ipsum sed
-          ultricies lobortis, mi magna sollicitudin erat, id pellentesque nulla
-          mi nec nunc. Sed a nisl non metus mattis tincidunt. Sed dignissim
-          neque non est blandit, sit amet commodo ligula congue. Phasellus
-          cursus sed eros sed sagittis.
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: body }} />
       </S.Body>
     </S.Content>
   )
