84fdaddab5d29e88d32b2b5e7fae215a9803cbc6
Márcio Vinícius <marcio.campos@outlook.com>
2021-04-18 17:17:18 -0300

    add GET_PAGES

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,15 @@
 import AboutTemplate from 'templates/About'
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 
-const About = () => {
+export default function AboutPage() {
   return <AboutTemplate />
 }
-export default About
+
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+  console.log(pages)
+  return {
+    props: {}
+  }
+}
